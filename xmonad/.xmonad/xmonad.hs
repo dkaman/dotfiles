89d201c0425e901c52ed013f8956136c0e5c4a36
Dallas Kaman <dallas.kaman@gmail.com>
2014-10-30 19:48:14 -0500

    now I added emacs xmonad and xmobar

---
+++ xmonad/.xmonad/xmonad.hs
@@ -0,0 +1,29 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    xmproc <- spawnPipe "xmobar /home/dallas/.xmobarrc"
+    xmonad $ defaultConfig
+        { terminal = myTerminal 
+        , workspaces = myWorkspaces
+        , focusedBorderColor = myFocusedBorderColor
+        , manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "purple" "" . shorten 50
+                        }
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        ]
+
+myTerminal = "xterm"
+myWorkspaces = ["1:main", "2:web", "3:emacs", "4:term", "5:media"]
+myFocusedBorderColor = "#85E0FF"

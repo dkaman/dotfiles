fbae05bd737ca0b6698a59ea13c6bd8055ae66fa
Dallas Kaman <dallas.kaman@gmail.com>
2014-10-31 19:10:46 -0500

    moved dirs around

--- emacs.d/emacs/elpa/evil-lisp-state-20141017.704/evil-lisp-state-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; evil-lisp-state-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (evil-lisp-state-backward-prefix) "evil-lisp-state"
-;;;;;;  "evil-lisp-state.el" (21575 7087 545433 300000))
-;;; Generated autoloads from evil-lisp-state.el
-
-(defvar evil-lisp-state-backward-prefix "<tab>" "\
-Prefix to execute the backward version of a command")
-
-(custom-autoload 'evil-lisp-state-backward-prefix "evil-lisp-state" t)
-
-;;;***
-
-;;;### (autoloads nil nil ("evil-lisp-state-pkg.el") (21575 7087
-;;;;;;  657327 21000))
-
-;;;***
-
-(provide 'evil-lisp-state-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; evil-lisp-state-autoloads.el ends here

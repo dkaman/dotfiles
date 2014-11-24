5d053f6be3f351102d2a8c2e2259a5e4ea5d05a4
Dallas Kaman <dallas.kaman@gmail.com>
2014-11-24 14:00:02 -0600

    stabilized scratchpads

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -175,7 +175,15 @@ window configuration."
       org-hierarchical-todo-statistics     nil
       org-hierarchical-checkbox-statistics nil
       org-src-fontify-natively t
-      org-directory "~/org")
+      org-directory "~/org") 
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '(;; other Babel languages
+   (plantuml . t)))
+
+(setq org-plantuml-jar-path
+      (expand-file-name "/home/dallas/Documents/EE460N/res/plantuml.jar"))
 
 (setq org-default-notes-file (concat org-directory "capture.org")) 
 (evil-leader/set-key "oc" 'org-capture) 
@@ -199,6 +207,65 @@ window configuration."
         ("m" "Music" entry
          (file+headline (concat org-directory "/music.org") "Notes")
          "* %?\n")))
+
+(eval-after-load "org-exp-blocks"
+ '(progn
+   (add-to-list 'org-export-blocks '(uml iy/org-export-blocks-format-plantuml nil))
+   (add-to-list 'org-protecting-blocks "uml")))
+
+ (defvar iy/org-plantuml-jar-path (expand-file-name "~/Dropbox/java-libs/plantuml.jar")
+   "Path to the plantuml jar executable.")
+ (defun iy/org-export-blocks-format-plantuml (body &rest headers)
+   "Pass block BODY to the plantuml utility creating an image.
+   Specify the path at which the image should be saved as the first
+   element of headers, any additional elements of headers will be
+   passed to the plantuml utility as command line arguments."
+   (message "plantuml-formatting...")
+   (let* ((args (if (cdr headers) (mapconcat 'identity (cdr headers) " ")))
+          (data-file (make-temp-file "org-plantuml"))
+          (hash (progn
+                  (set-text-properties 0 (length body) nil body)
+                  (sha1 (prin1-to-string (list body args)))))
+          (raw-out-file (if headers (car headers)))
+          (out-file-parts (if (string-match "\\(.+\\)\\.\\([^\\.]+\\)$" raw-out-file)
+                              (cons (match-string 1 raw-out-file)
+                                    (match-string 2 raw-out-file))
+                            (cons raw-out-file "png")))
+          (out-file (concat (car out-file-parts) "_" hash "." (cdr out-file-parts))))
+     (unless (file-exists-p iy/org-plantuml-jar-path)
+       (error (format "Could not find plantuml.jar at %s" iy/org-plantuml-jar-path)))
+     (setq body (if (string-match "^\\([^:\\|:[^ ]\\)" body)
+                    body
+                  (mapconcat (lambda (x) (substring x (if (> (length x) 1) 2 1)))
+                             (org-split-string body "\n")
+                             "\n")))
+     (cond
+      ((or htmlp latexp docbookp)
+       (unless (file-exists-p out-file)
+         (mapc ;; remove old hashed versions of this file
+          (lambda (file)
+            (when (and (string-match (concat (regexp-quote (car out-file-parts))
+                                             "_\\([[:alnum:]]+\\)\\."
+                                             (regexp-quote (cdr out-file-parts)))
+                                     file)
+                       (= (length (match-string 1 out-file)) 40))
+              (delete-file (expand-file-name file
+                                             (file-name-directory out-file)))))
+          (directory-files (or (file-name-directory out-file)
+                               default-directory)))
+         (with-temp-file data-file (insert (concat "@startuml\n" body "\n@enduml")))
+         (message (concat "java -jar " iy/org-plantuml-jar-path " -pipe " args))
+         (with-temp-buffer
+           (call-process-shell-command
+            (concat "java -jar " iy/org-plantuml-jar-path " -pipe " args)
+            data-file
+            '(t nil))
+           (write-region nil nil out-file)))
+       (format "\n[[file:%s]]\n" out-file))
+      (t (concat
+          "\n#+BEGIN_EXAMPLE\n"
+          body (if (string-match "\n$" body) "" "\n")
+          "#+END_EXAMPLE\n")))))
 
 (require-package 
 '(evil
--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,6 +1,7 @@
 
 import XMonad
 import System.IO
+import qualified XMonad.StackSet as W
 
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
@@ -8,12 +9,15 @@ import XMonad.Hooks.FloatNext
 
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig
+import XMonad.Util.NamedScratchpad
 
 import qualified XMonad.Actions.Search as S
 import XMonad.Actions.CycleWS
 import XMonad.Actions.WindowBringer
 import XMonad.Actions.WindowGo
 import XMonad.Actions.Volume
+import XMonad.Actions.DynamicWorkspaces
+import XMonad.Actions.DynamicWorkspaceGroups
 
 import XMonad.Layout.Spacing
 import XMonad.Layout.NoBorders
@@ -33,11 +37,11 @@ myAccent = "#85E0FF"
 
 myBar = "xmobar /home/dallas/.xmobarrc"
 myBorderWidth = 2
-myWorkspaces = ["1:main" 
-               ,"2:web" 
-               ,"3:emacs" 
-               ,"4:term" 
-               ,"5:media"
+myWorkspaces = ["1" 
+               ,"2"
+               ,"3"
+               ,"4"
+               ,"5"
                ]
 
 main = do
@@ -47,7 +51,7 @@ main = do
         , workspaces = myWorkspaces
         , focusedBorderColor = myAccent
         , borderWidth = myBorderWidth
-        , manageHook =  manageHook defaultConfig <+> manageDocks <+> myManageHook
+        , manageHook =  myManageHook
         , layoutHook = avoidStruts $ 
                        smartBorders $ 
                        spacing 2 $ 
@@ -56,20 +60,17 @@ main = do
         , logHook = myLogHook h
         , modMask = mod4Mask 
         } 
-        `removeKeysP` ["M4-" ++ n | n <- unwantedKeys ]
         `additionalKeysP` addedKeys
+
+myScratchpads = [ NS "alsamixer" "xterm -e alsamixer" (title =? "alsamixer") (customFloating $ W.RationalRect 0.6 0.1 0.35 0.5)
+                , NS "xterm" "xterm -name scratchterm" (title =? "scratchterm") (customFloating $ W.RationalRect 0.05 0.8 0.9 0.15)
+                ]
 
 myStartup = do
     spawn "emacs --daemon"
     spawn "xmodmap ~/.xmodmap"
     spawn "xscreensaver -no-splash &"
     spawn "xterm"
-
-unwantedKeys = ["p"  -- You guys should be ashamed of yourselves
-               ,"n"
-               ,"<Tab>" 
-               ,"<Return>"
-               ]
 
 addedKeys = [("M4-r", spawn "dmenu_run")
             ,("M4-=", refresh) -- why doesn't this work?
@@ -78,13 +79,15 @@ addedKeys = [("M4-r", spawn "dmenu_run")
             ,("M4-<Tab>", toggleWS)
             ,("M4-s M4-l", spawn "xscreensaver-command --lock")
             ,("M4-s M4-s", spawn "scrot ~/Documents/screenshots/%Y-%m-%d-%T-screenshot.png")
-            ,("M4-<Return>", spawn "xterm -e /home/dallas/scripts/screen.sh")
             ,("M4-w g", gotoMenu)
             ,("M4-w b", bringMenu)
             ,("M4-<F1>", runOrRaise "emacsclient -c -a emacs " (className =? "Emacs"))
             ,("M4-<F2>", runOrRaise "uzbl-tabbed" (className =? "Uzbl-tabbed"))
-            ,("M4-<F12>", spawn "xterm -e alsamixer")
+            ,("M4-<F12>", namedScratchpadAction myScratchpads "alsamixer")
             ,("M4-S-<F12>", spawn "amixer -D pulse set Master toggle")
+            ,("M4-x M4-k", spawn "xkill")
+            ,("M4-<Return>", namedScratchpadAction myScratchpads "xterm")
+            ,("M4-S-<Return>", spawn "xterm -e /home/dallas/scripts/screen.sh")
             ] 
             -- Search functionality (thanks tylevad on Github!)
             ++ [("M4-s " ++ k, S.promptSearchBrowser myXPConfig myBrowser f) | (k,f) <- searchEngines]
@@ -95,17 +98,18 @@ addedKeys = [("M4-r", spawn "dmenu_run")
                                      , ("a", S.searchEngine "ArchWiki" "http://wiki.archlinux.org/index.php/Special:Search?search=")
                                      ]
 
-myManageHook = composeAll [
+myManageHook = composeAll [ manageDocks
+                          , namedScratchpadManageHook myScratchpads
                           ]
 
-myLogHook h = (dynamicLogWithPP $ myPP h)
+myLogHook h = (dynamicLogWithPP . namedScratchpadFilterOutWorkspacePP $ myPP h)
 
 myPP h = xmobarPP
   { ppCurrent         = xmobarColor myEmpty "" . wrap "{" "}" . xmobarColor myHighlight ""
   , ppVisible         = xmobarColor myEmpty "" . wrap "[" "]" . xmobarColor myVisible ""
   , ppHidden          = xmobarColor myForeground ""
   , ppHiddenNoWindows = xmobarColor myEmpty ""
-  , ppTitle           = xmobarColor myForeground "" . shorten 130
+  , ppTitle           = xmobarColor myForeground "" . shorten 100
   , ppLayout          = xmobarColor myAccent ""
   , ppSep             = " <fc=" ++ myLowlight ++ ">|</fc> "
   , ppWsSep           = " "
